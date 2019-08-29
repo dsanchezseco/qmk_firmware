537a24076374b93dd9f31b32156cd9d3ad037903
dsanchezseco <dsanchezseco>
2019-08-29 10:28:28 +0200

    changed led matrix to regular led light

--- keyboards/crkbd_i2c/keymaps/dsanchezseco/config.h
+++ keyboards/crkbd_i2c/keymaps/dsanchezseco/config.h
@@ -21,20 +21,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 //#define USE_MATRIX_I2C
+#define USE_SERIAL
 
 /* Select hand configuration */
 
 // #define MASTER_LEFT
 // #define MASTER_RIGHT
 #define EE_HANDS
-/*
-#ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
-*/
+
+// RGB EFFECTS
+#define RGBLIGHT_EFFECT_STATIC_LIGHT
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RGB_TEST
--- keyboards/crkbd_i2c/keymaps/dsanchezseco/rules.mk
+++ keyboards/crkbd_i2c/keymaps/dsanchezseco/rules.mk
@@ -2,12 +2,14 @@
 # If you want to change the display of OLED, you need to change here
 SRC += ./logo_reader.c \
 
+# enable OLED displays
 OLED_DRIVER_ENABLE = yes
 
 # enable media keys
 EXTRAKEY_ENABLE = yes
 
-RGB_MATRIX_ENABLE = WS2812
+# enable LEDs
+RGBLIGHT_ENABLE = yes
 
 # using elite-c controllers
 BOOTLOADER = qmk-dfu
